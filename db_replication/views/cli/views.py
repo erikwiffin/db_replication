d022f40d04aa4e2a3eb2c2ca403c7e3dd2427920
Erik Wiffin <erik@devetry.com>
2019-11-03 21:26:19 -0700

    More sample data

--- db_replication/views/cli/views.py
+++ db_replication/views/cli/views.py
@@ -1,6 +1,7 @@
 # pylint: disable=no-member
 from glob import glob
 from pathlib import Path
+from random import sample
 
 import click
 from flask import Blueprint
@@ -31,21 +32,28 @@ def initdb():
     user3 = User("carol", "carol@example.com")
     user4 = User("carlos", "carlos@example.com")
     user5 = User("charlie", "charlie@example.com")
+    user6 = User("dave", "dave@example.com")
     user1.follow(user2)
     user2.follow(user3)
     user2.follow(user4)
     user2.follow(user5)
+    user5.follow(user6)
 
     posts_dir = Path(BP.root_path) / 'posts'
+    posts = []
     for path in posts_dir.glob('*.txt'):
         with open(path) as fh:
             for i, line in enumerate(fh):
-                user1.write_post(f'{ Path(path).name } { i }', line)
+                posts.append((f'{ Path(path).name } { i }', line))
+
+    for title, body in sample(posts, len(posts)):
+        user1.write_post(title, body)
 
     db.session.add(user1)
     db.session.add(user2)
     db.session.add(user3)
     db.session.add(user4)
     db.session.add(user5)
+    db.session.add(user6)
     db.session.commit()
 
